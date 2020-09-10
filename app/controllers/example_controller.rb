8ecb8a57f4764a8e1dc7deb09b62604fe2fb83d1
Diego Romero <diego.romero@pedidosya.com>
2020-09-10 09:00:29 -0300

    Uncomment redis

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -12,16 +12,17 @@ class ExampleController < ApplicationController
   
   # STEP 2
 
-  # def cual_es_mi_nombre_redis
-  #   param_my_name = params[:name]
+  def cual_es_mi_nombre_redis
+    param_my_name = params[:name]
 
-  #   if param_my_name
-  #     Rails.cache.write('name', param_my_name)
-  #   end
-  #   my_name = Rails.cache.fetch('name')
+    if param_my_name
+      Rails.cache.write('name', param_my_name)
+    end
+    
+    my_name = Rails.cache.fetch('name')
 
-  #   render json: { message: my_name }
-  # end
+    render json: { message: my_name }
+  end
 
   # STEP 3
 
