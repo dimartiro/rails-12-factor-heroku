ce2f94d2d1683592880bc65c80e391b91df8b104
Diego Romero <diego.romero@pedidosya.com>
2020-09-10 01:02:18 -0300

    Commit steps

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -3,28 +3,33 @@ class ExampleController < ApplicationController
     render json: { message: 'welcome!' }
   end
 
-  def cual_es_mi_nombre_var
-    my_name = ENV.fetch('NAME')
+  # STEP 1
 
-    render json: { message: my_name }
-  end
+  # def cual_es_mi_nombre_var
+  #   my_name = ENV.fetch('NAME')
 
-  def cual_es_mi_nombre_redis
-    param_my_name = params[:name]
+  #   render json: { message: my_name }
+  # end
+  
+  # STEP 2
 
-    if param_my_name
-      Rails.cache.write('name', param_my_name)
-    end
+  # def cual_es_mi_nombre_redis
+  #   param_my_name = params[:name]
 
-    my_name = Rails.cache.fetch('name')
+  #   if param_my_name
+  #     Rails.cache.write('name', param_my_name)
+  #   end
+  #   my_name = Rails.cache.fetch('name')
 
-    render json: { message: my_name }
-  end
+  #   render json: { message: my_name }
+  # end
 
-  def cual_es_mi_nombre_global
-    $name_from_global = params[:name] if params[:name]
-    name_from_global = $name_from_global || 'not defined'
-    render json: { name: name_from_global }
-  end
+  # STEP 3
+  
+  # def cual_es_mi_nombre_global
+  #   $name_from_global = params[:name] if params[:name]
+  #   name_from_global = $name_from_global || 'not defined'
+  #   render json: { name: name_from_global }
+  # end
 
 end
